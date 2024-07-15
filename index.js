2cfc79ee5376686a106a6791c81999e94bea86f0
Jakub Smid <jakubsmid22@gmail.com>
2024-07-15 13:10:43 +0200

    Js done

---
+++ index.js
@@ -0,0 +1,139 @@
+const buttons = document.querySelectorAll("[role='button']");
+const display = document.getElementById("displayBottom");
+const displayTop = document.getElementById("displayTop");
+let resultOnDisplay = false;
+let topValue = 0;
+let bottomValue = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+
+    if (resultOnDisplay) {
+        resultOnDisplay = !resultOnDisplay;
+        display.textContent = "";
+        displayTop.textContent = "";
+    }
+
+    switch (buttonValue) {
+      case "AC":
+        display.textContent = "";
+        displayTop.textContent = "";
+        break;
+
+      case "+/-":
+        if (display.textContent) {
+          if (display.textContent[0] === "+") {
+            display.textContent = "-" + display.textContent.slice(1);
+          } else if (display.textContent[0] === "-") {
+            display.textContent = "+" + display.textContent.slice(1);
+          } else if (
+            display.textContent[0] !== "+" &&
+            display.textContent !== "0" &&
+            display.textContent !== ""
+          ) {
+            display.textContent = "+".concat(display.textContent);
+            console.log(display.textContent);
+          } else {
+            display.textContent = "-".concat(display.textContent);
+          }
+        }
+        break;
+
+      case "%":
+        if (topValue && bottomValue) {
+          bottomValue = bottomValue / 100;
+          displayTop.textContent = `${topValue} + ${bottomValue} =`;
+          display.textContent = topValue + bottomValue;
+          resultOnDisplay = true;
+        }
+        break;
+
+      case "/":
+      case "x":
+      case "+":
+      case "-":
+        if (display.textContent) {
+          switch (displayTop.textContent.slice(-1)) {
+            case "/":
+            case "x":
+            case "+":
+            case "-":
+                handleOperation(getOperation(displayTop.textContent.slice(-1)), " " + buttonValue);
+                break;
+            default:
+              topValue = parseFloat(display.textContent);
+              displayTop.textContent = display.textContent + " " + buttonValue;
+              display.textContent = "";
+              break;
+          }
+        }
+        break;
+
+      case ".":
+        if (!display.textContent.includes(".") && display.textContent) {
+          display.textContent += buttonValue;
+        }
+        break;
+
+      case "=":
+        const regex = /^(\d+\.?\d*)\s*([\/x+-])\s*$/;
+        const match = displayTop.textContent.match(regex);
+        if (match) {
+          topValue = parseFloat(match[1]);
+          bottomValue = parseFloat(display.textContent);
+          displayTop.textContent = "";
+          resultOnDisplay = true;
+          
+          switch (match[2]) {
+            case "+":
+              display.textContent = `${add(topValue, bottomValue)}`;
+              break;
+            case "-":
+              display.textContent = `${subtract(topValue, bottomValue)}`;
+              break;
+            case "/":
+              display.textContent = `${divide(topValue, bottomValue)}`;
+              break;
+            case "x":
+              display.textContent = `${multiply(topValue, bottomValue)}`;
+              break;
+            default:
+              display.textContent = "error";
+              resultOnDisplay = false;
+          }
+          topValue = 0;
+          bottomValue = 0;
+        } else {
+          display.textContent = "error";
+        }
+        break;
+
+      default:
+        display.textContent += buttonValue;
+        break;
+    }
+  });
+});
+
+const divide = (x, y) => x / y;
+const multiply = (x, y) => x * y;
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+
+const handleOperation = (operation, operator) => {
+  bottomValue = parseFloat(display.textContent);
+  display.textContent = "";
+  displayTop.textContent = operation(topValue, bottomValue) + operator;
+  topValue = parseFloat(displayTop.textContent);
+};
+
+const getOperation = (operator) => {
+  switch (operator) {
+    case "/": return divide;
+    case "x": return multiply;
+    case "+": return add;
+    case "-": return subtract;
+    default: return null;
+  }
+};
